b0a529ada1375412ffb037951a44049fa14e6521
yuu <3334919257@qq.com>
2024-07-22 10:09:49 +0800

    test creat

---
+++ FCqiugenTest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	float a, b, c, x1, x2;
+	cout << "请输入系数a,b和常数c" << endl;
+	cin >> a >> b >> c;
+	if ((b * b - 4 * a * c) >= 0) {
+		x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+		cout << "方程有1或2个实数根分别为" << endl;
+		cout << "x1=" << x1 << endl << "x2=" << x2;
+		/*	else if ((b * b - 4 * a * c) == 0)
+				x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+			cout << "返程有1个实数根为" << endl;
+			cout << x1;*/
+	} else
+		cout << "方程无实数根";
+
+	return 0;
+}
+
